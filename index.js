89fe9d0d273f56229e897846448eb6efed1a8964
Green Camper <stefan.palacios@gmail.com>
2023-04-17 14:30:58 -0500

    Added schema to schema.sql, started create and view functions for index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 const dotenv = require("dotenv");
+const { default: inquirer } = require("inquirer");
 dotenv.config();
 
 const app = express();
@@ -24,7 +25,8 @@ const db = mysql.createConnection(
 
 //inquirer start, ask user how to start
 
-inquirer
+function start () {
+  inquirer
   .prompt([
     {
       type: "list",
@@ -62,10 +64,93 @@ inquirer
         process.exit();
     }
   });
-
+}
+  
 //create a new employee function (post)
+function createEmployee() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: 'Enter employee first name:',
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: 'Enter employee last name:',
+    },
+    {
+      type: 'input',
+      name: 'roleId',
+      message: 'Enter employee role id:',
+    },
+    {
+      type: 'input',
+      name: 'departmentId',
+      message: 'Enter employee department id:',
+    },
+  ]).then((answer) => {
+    switch (answer.action) {
+      case "View all employees":
+        const sql = `SELECT * FROM employees`;
+        db.query(sql, (err, rows) => {
+          if (err) {
+            res.status(500).json({ error: err.message });
+             return;
+          }
+          res.json({
+            message: 'success',
+            data: rows
+          });
+        });
+        break;
+      case "Create a new employee":
+        createEmployee();
+        break;
+
+    }
+  })
+
+};
 
 //read the database and view employees, roles, and departments (get)
+function createEmployee() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'task',
+      message: 'What would you like to view?',
+      choices: [
+        'View all employees',
+        'View all roles',
+        'View all departments',
+        'View employees by department',
+        'View employees by manager'
+      ]
+    }
+  ]).then((answer) => {
+    switch (answer.action) {
+      case "View all employees":
+        const sql = `SELECT * FROM employees`;
+        db.query(sql, (err, rows) => {
+          if (err) {
+            res.status(500).json({ error: err.message });
+             return;
+          }
+          res.json({
+            message: 'success',
+            data: rows
+          });
+        });
+        break;
+      case "Create a new employee":
+        createEmployee();
+        break;
+
+    }
+  })
+
+};
 //delete an employee from database (delete)
 //update info for an employee (update)
 //view employee by manager (get)
