7aa13d903a8a245a2c2edbd14489dee630096a6e
Green Camper <stefan.palacios@gmail.com>
2023-04-16 22:25:09 -0500

    Init modules and express middleware

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 //import modules (express, mysql2, inquirer)
+const express = require('express');
+const mysql = require('mysql2');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
 //express middleware (urlencoded/url parser, json parser)
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //database connection (mysql)
 
 //inquirer start, ask user how to start
 //create a new employee function (post)
+
 //read the database and view employees, roles, and departments (get)
 //delete an employee from database (delete)
 //update info for an employee (update)
