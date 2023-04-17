b44724d72167d71fddabd0dc4853593bda8a9c37
Green Camper <stefan.palacios@gmail.com>
2023-04-16 22:21:51 -0500

    Started pseudocoding for server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+//import modules (express, mysql2, inquirer)
+//express middleware (urlencoded/url parser, json parser)
+
+//database connection (mysql)
+
+//inquirer start, ask user how to start
+//create a new employee function (post)
+//read the database and view employees, roles, and departments (get)
+//delete an employee from database (delete)
+//update info for an employee (update)
+//view employee by manager (get)
