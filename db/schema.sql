3aac31a3a2f5eae8305ad571e0ef583d447f13cc
Green Camper <stefan.palacios@gmail.com>
2023-04-18 21:47:54 -0500

    Added comments and UNIQUE constraint to department names

--- db/schema.sql
+++ db/schema.sql
@@ -2,17 +2,18 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
---manager table holds the names and ids of the managers
+
+/*manager table holds the names and ids of the managers*/
 CREATE TABLE manager (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
---department table holds the names and ids of all departments
+/*department table holds the names and ids of all departments*/
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL UNIQUE
 );
---role table holds the names of roles, salaries for this role and the department its contained in
+/*role table holds the names of roles, salaries for this role and the department its contained in*/
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
@@ -22,7 +23,7 @@ CREATE TABLE role (
     REFERENCES department(id)
     ON DELETE SET NULL
 );
---employee table holds the employee details, and ties them to the manager and role tables
+/*employee table holds the employee details, and ties them to the manager and role tables*/
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
