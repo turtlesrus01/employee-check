9294207b844d2f424a956924a5980398206af539
Green Camper <stefan.palacios@gmail.com>
2023-04-18 21:35:29 -0500

    Added manager table to hold names and edited data types on employee table

--- db/schema.sql
+++ db/schema.sql
@@ -2,12 +2,17 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
-
+--manager table holds the names and ids of the managers
+CREATE TABLE manager (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+--department table holds the names and ids of all departments
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
-
+--role table holds the names of roles, salaries for this role and the department its contained in
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
@@ -17,12 +22,12 @@ CREATE TABLE role (
     REFERENCES department(id)
     ON DELETE SET NULL
 );
-
+--employee table holds the employee details, and ties them to the manager and role tables
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  manager_id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  manager_id INT,
   role_id INT,
   FOREIGN KEY (manager_id)
     REFERENCES employee(id)
