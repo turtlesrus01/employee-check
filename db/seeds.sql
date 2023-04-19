46dc5d94b09c3ca8acf8a229df44c69e70ee5a05
Green Camper <stefan.palacios@gmail.com>
2023-04-18 22:12:51 -0500

    Finished seeds.sql, included at least three employees data

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,10 +2,20 @@
 INSERT INTO manager (name) VALUES ('Juan Doe');
 INSERT INTO manager (name) VALUES ('Juanita Cruz');
 INSERT INTO manager (name) VALUES ('Edward Martinez');
-INSERT INTO manager (name) VALUES ('Edward Martinez');
+INSERT INTO manager (name) VALUES ('Walter White');
 
 /*departments*/
 INSERT INTO department (name) VALUES ('Sales');
 INSERT INTO department (name) VALUES ('Marketing');
 INSERT INTO department (name) VALUES ('Engineering');
+
+/*roles*/
+INSERT INTO role (title_id, salary, department_id) VALUES ('Sales Executive', 50000, 1);
+INSERT INTO role (title_id, salary, department_id) VALUES ('Marketing Manager', 60000, 2);
+INSERT INTO role (title_id, salary, department_id) VALUES ('Software Engineer', 80000, 3);
+
+/*employees*/
+INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUES ('Jesse', 'Pinkman', 4, 3);
+INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUES ('Alice', 'Lee', 2, 2);
+INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUES ('David', 'Wang', 1, 3);
 
