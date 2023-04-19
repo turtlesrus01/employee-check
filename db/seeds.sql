b705e627eb9d33ebe703baa9527e3b14c9010ee8
Green Camper <stefan.palacios@gmail.com>
2023-04-18 23:08:30 -0500

    Fixed small errors in definition

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,12 +10,12 @@ INSERT INTO department (name) VALUES ('Marketing');
 INSERT INTO department (name) VALUES ('Engineering');
 
 /*roles*/
-INSERT INTO role (title_id, salary, department_id) VALUES ('Sales Executive', 50000, 1);
-INSERT INTO role (title_id, salary, department_id) VALUES ('Marketing Manager', 60000, 2);
-INSERT INTO role (title_id, salary, department_id) VALUES ('Software Engineer', 80000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ('Sales Executive', 50000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Marketing Manager', 60000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 80000, 3);
 
 /*employees*/
-INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUES ('Jesse', 'Pinkman', 4, 3);
-INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUES ('Alice', 'Lee', 2, 2);
-INSERT INTO employees (first_name, last_name, manager_id, role_id) VALUES ('David', 'Wang', 1, 3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Jesse', 'Pinkman', 4, 3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('Alice', 'Lee', 2, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES ('David', 'Wang', 1, 3);
 
